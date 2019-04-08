60784fab0235e9dacf015e0d312e00a080dbdc75
Taiwoo Park <taiwoo.park@gmail.com>
2019-04-08 09:08:58 -0700

    Prepared to demonstrate image loading using a resource file structure
    (not implemented yet!)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    loadImage();
+}
+
+void MainWindow::loadImage() {
+    QString bearFileName = "images/bears.jpg";
+
+    if(imageBears.load(bearFileName)) {
+        std::cout << "Bear image loaded successfully" << std::endl;
+        imageBears = imageBears.scaled(ui->label->size(), Qt::KeepAspectRatioByExpanding);
+    }
+
+    ui->label->setPixmap(imageBears);
 }
 
 MainWindow::~MainWindow()
